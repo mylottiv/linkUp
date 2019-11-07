7a757c202467ea9668f5b44b84e41b39a5f6cffe
Thomas Mylott <mylottiv@gmail.com>
2019-11-07 14:37:09 -0600

    Moving chat client js code to seperate file in public dir

---
+++ socketsDemo/public/js/chatClient.js
@@ -0,0 +1,71 @@
+$(function() {
+        
+    // Toggle Materialize input labels
+    M.updateTextFields();
+
+    // Capture clientRoom from header
+    const clientRoomName = $('#roomname').text();
+
+    // Initialize socket connection
+    const socket = io('http://localhost:3000');
+    socket.on('connect', function() {
+
+        console.log('Client socket connected');
+
+        // Join client to current chatroom
+        socket.emit('join', clientRoomName);
+
+
+        // Client-side new message listener
+        socket.on('new message', function(newMessage) {
+
+            console.log('New message:', newMessage);
+            const {user, content} = newMessage;
+
+            // Reset user typing message if message not from client user
+            // if (user !== clientUser) {
+                $('#currently-typing').text('');
+            // };
+
+            // Add new chat message card to chat box
+            $('#chat-box').html($('#chat-box').html() + `<li class='card'><div class='card-content'>${user}: ${content}</div></li>`);
+        
+        });
+
+        // Client-side listener for other users typing event
+        socket.on('typing', function(typingUser) {
+
+            console.log(typingUser, 'is typing');
+
+            // Update currently typing notification
+            $('#currently-typing').text(`${typingUser} is currently typing`);
+
+        })
+    });
+
+    // Handler for new message form submission
+    $('#chat-submission').submit(function(e) {
+
+        e.preventDefault();
+
+        // Assemble message data and emit as new message event
+        const data = {
+            roomName: clientRoomName,
+            user: $('#user').val(),
+            content: $('#message').val()
+        };
+        socket.emit('new message', data);
+
+    });
+
+    // Handler for user typing event
+    $('#message').keyup(function(e) {
+
+        e.preventDefault();
+        console.log('keyup fired');
+
+        // Emit typing event with user name and room name
+        socket.emit('typing', {user: $('#user').val(), roomName: clientRoomName});
+
+    });
+});
