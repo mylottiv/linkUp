23167f43497cdf53f65accff2e75797154f8cdb9
Thomas Mylott <mylottiv@gmail.com>
2019-11-06 23:58:30 -0600

    Implemented Handelbars views delivery system as groundwork for multiple chatrooms

--- socketsDemo/server.js
+++ socketsDemo/server.js
@@ -1,11 +1,53 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT);
 const path = require('path');
 const io = require('socket.io').listen(server);
+const exphbs = require('express-handlebars');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'chat.html'));
+// Init Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Init express handlebars engine
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Init chatroom object
+const Chatrooms = {
+    rooms: [
+        {roomName: "Showdown"}
+    ],
+}
+
+// Home get controller
+app.get('/:home?', function(req, res, next) {
+
+    // Serve home view if route goes to root, "home", or "index"
+    if (req.params.home === 'home' || req.params.home === 'index' || !req.params.home) {
+        res.render("home", Chatrooms);
+    }
+
+    // Otherwise fall through to chat room route controller
+    else {
+        next();
+    };
+
+})
+
+// Chatroom get controller
+app.get('/:chat', function(req, res, next) {
+
+    // Serve chatroom view for room name from route
+    res.render('chat', {roomName: req.params.chat});
+
 })
 
 // Establish server connection
