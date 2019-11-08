205774e846cfd289ec07cfff54691d13eb7ba2e9
Jerry Castro <castrojerry23@gmail.com>
2019-11-07 18:48:57 -0600

    added side navbar to main html, css to structure navbar and onclick function to javascript file

--- models/index.js
+++ models/index.js
@@ -40,3 +40,18 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+function myFunc(e){
+  e.classList.toggle("show");
+
+  var elem = document.getElementById("navID"),
+  style = window.getComputedStyle(elem),
+  top = style.getPropertyValue("right");
+
+  if(top == "0px"){
+      elem.style.right = "-260px";
+  }else{
+      elem.style.right = "0px";
+  }
+
+}
