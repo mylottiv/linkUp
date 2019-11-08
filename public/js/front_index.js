b9d75d53d6aeda008162edd8c66baf3ffd2ce689
Jerry Castro <castrojerry23@gmail.com>
2019-11-07 19:30:17 -0600

    changed public folder index.js to front_index.js and moved side navbar onclick function to front_index.js file.

--- public/js/front_index.js
+++ public/js/front_index.js
@@ -97,3 +97,18 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+function myFunc(e){
+  e.classList.toggle("show");
+
+  var elem = document.getElementById("navID"),
+  style = window.getComputedStyle(elem),
+  top = style.getPropertyValue("right");
+
+  if(top == "0px"){
+      elem.style.right = "-260px";
+  }else{
+      elem.style.right = "0px";
+  }
+
+}
